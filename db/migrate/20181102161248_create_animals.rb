1cccafac4682f4dd39ba12e32b1b72c57efb69aa
Carl Adolfson <cadolfson@outlook.com>
2018-11-02 11:17:12 -0500

    Add models for category animal and product

---
+++ db/migrate/20181102161248_create_animals.rb
@@ -0,0 +1,15 @@
+class CreateAnimals < ActiveRecord::Migration[5.2]
+  def change
+    create_table :animals do |t|
+      t.string :name
+      t.text :description
+      t.decimal :cost
+      t.string :image
+      t.string :breed
+      t.boolean :sold
+      t.references :category, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
