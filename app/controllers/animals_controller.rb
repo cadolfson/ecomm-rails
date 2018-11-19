547e5ab289c70084ab59093d1abd3706847d5dcc
Carl Adolfson <cadolfson@outlook.com>
2018-11-19 10:24:33 -0600

    Product and animal controller

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class AnimalsController < ApplicationController
   def index
+    @animals = Animal.order(:name).page(params[:page]).per(5)
   end
 
   def show
+    @animal = Animal.find(params[:id])
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class ProductsController < ApplicationController
   def index
+    @products = Product.order(:name).page(params[:page]).per(5)
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 end
