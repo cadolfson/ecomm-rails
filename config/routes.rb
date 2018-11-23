8b601d3ef21e2b18d773e5a51239f98ca3cd1e5b
Carl Adolfson <cadolfson@outlook.com>
2018-11-23 13:00:10 -0600

    Implement bootstrap nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'search/index'
+  get 'search/results'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
